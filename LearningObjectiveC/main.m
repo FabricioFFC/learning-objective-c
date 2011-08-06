e2dc0308ecb3ddab02e3d8b7375ea44270b78714
Fabrício Ferrari de Campos <ffc.fabricio@gmail.com>
2011-08-06 19:36:34 -0300

    Initial Commit

---
+++ LearningObjectiveC/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  LearningObjectiveC
+//
+//  Created by Fabricio Campos on 8/6/11.
+//  Copyright 2011 F1 Studio. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+    // insert code here...
+    NSLog(@"Hello, World!");
+
+    [pool drain];
+    return 0;
+}
+
